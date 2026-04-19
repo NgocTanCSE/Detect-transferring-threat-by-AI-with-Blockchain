4825dd7004728c7eb4828c83a6977331ca5ab310
Lê Ngọc Tân <ngoctan2le5@gmail.com>
2026-04-19 18:13:50 +0700

    refactor: clean up AdminDashboardLegacyRedirect by removing unused imports and code

--- frontend/src/app/admin/dashboard/page.tsx
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -1,31 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import {
-  Droplets,
-  TrendingUp,
-  import { redirect } from "next/navigation";
-
-  export default function AdminDashboardLegacyRedirect() {
-    redirect("/?role=system_admin&feature=0");
-  }
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </TableCell>
-                        </TableRow>
-                      )}
-                    </>
-                  );
-                })
-              )}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </div>
-  );
+export default function AdminDashboardLegacyRedirect() {
+  redirect("/?role=system_admin&feature=0");
 }
