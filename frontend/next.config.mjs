ffa412b1ff67d9c9bd9a20afd035239a85f9ed48
Lê Ngọc Tân <ngoctan2le5@gmail.com>
2026-04-25 21:47:13 +0700

    feat: migrate to microservices architecture with RabbitMQ and multiple PostgreSQL instances and add frontend Docker configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  swcMinify: false,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
